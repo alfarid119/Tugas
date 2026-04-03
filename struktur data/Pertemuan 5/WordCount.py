487d2ff5c4da7b1e0536befc9c8927972a404e8f
alfarid119 <alfaridmaulani4@gmail.com>
2026-04-03 21:31:41 +0700

    update wordcount

--- struktur data/Pertemuan 5/WordCount.py
+++ struktur data/Pertemuan 5/WordCount.py
@@ -1,3 +1,19 @@
 import streamlit as st
 
 st.title("Word Count App")
+textInput = st.text_area("Masukkan teks Anda di sini:")
+
+if st.button("Hitung Kata"):
+    words = (textInput.split())
+    #masing-masing kata dipisah menggunakan split() 
+    #dihitung jumlahnya menggunakan len()
+    #simpan didalam dictionary 
+    counts = {}
+
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+    st.subheader("Frekuensi Kata")
+    st.write(counts)
+
+    st.bar_chart(counts)
